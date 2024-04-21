f6e2944a47986f0bf0889dc92d681429bee3c901
Arpit <arpit.vaghela@outlook.com>
2024-04-20 21:50:57 -0400

    add navbar and mobile view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/bloom.png"
+          alt="Bloom Logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-semibold text-center text-white max-sm:hidden">
+          Bloom
+        </p>
+      </Link>
+
+      <div className="flex flex-between gap-5">
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
   const pathName = usePathname();
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-w-sm:hidden lg:w-[264px]">
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
             pathName === link.route || pathName.startsWith(`${link.route}/`);
