abf40d8b929ff29ffd967ca1289ef906aa9a3670
Arpit <arpit.vaghela@outlook.com>
2024-04-20 21:12:15 -0400

    add sidebar and images

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 type HomeLayoutType = {
@@ -7,9 +9,9 @@ type HomeLayoutType = {
 const HomeLayout = ({ children }: HomeLayoutType) => {
   return (
     <main className="relative">
-      Navbar
+      <Navbar />
       <div className="flex">
-        Sidebar
+        <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
